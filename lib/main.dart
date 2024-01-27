c3c388e0a9f72e6d8a010bc0f603935f1b3595ac
Aditi Goyal <145578961+goyal02aditi@users.noreply.github.com>
2024-01-27 14:23:07 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
+
+import 'signup_page.dart';
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: HomePage(),
+      home: SignUpPage(),
     );
   }
 }
